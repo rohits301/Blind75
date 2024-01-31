20dd26fa52b75b47920b17dab177d8f2592ca3c8
Rohit Sachdeve <sachdevarohit5@gmail.com>
2024-01-31 23:19:26 +0530

    Added Brute Force approach for Top K freq. elements

--- Arrays And Hashing/TopKFrequentElements.java
+++ Arrays And Hashing/TopKFrequentElements.java
@@ -1,4 +1,31 @@
 class Solution {
+    // BRUTE FORCE
+    // T: O(k*n), S: O(n)
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        int[] ans = new int[k];
+
+        for (int i = 0; i < k; i++) {
+            int maxVal = 0;
+            int maxKey = 0;
+            for (int key : map.keySet()) {
+                if (map.get(key) > maxVal) {
+                    maxVal = map.get(key);
+                    maxKey = key;
+                }
+            }
+            ans[i] = maxKey;
+            map.remove(maxKey);
+        }
+        return ans;
+    }
+}
+class Solution {
+    // BETTER
     // T: O(k log D), S: O(D + D = D), D = no. of distinct elements, time to build heap is O(D)
     public int[] topKFrequent(int[] nums, int k) {
         
