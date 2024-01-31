7ff2de33c348aee79b05d425bbb737f30d5ad3e4
Rohit Sachdeve <sachdevarohit5@gmail.com>
2024-02-01 01:31:41 +0530

    Added Brute force approach for ProductOfArrayExceptSelf and LongestConsecSequence

--- Arrays And Hashing/LongestConsecutiveSequence.java
+++ Arrays And Hashing/LongestConsecutiveSequence.java
@@ -1,7 +1,65 @@
 class Solution {
-    // STRIVER's Video for reference
-    // BF - O(n^3), Better - O(nlogn), Optimal - O(n) time and space both
-    // the iteration in inner while is max. n so O(2n = n) time
+    // BRUTE FORCE
+    // T: O(n^3), S: O(1) 
+    // TLE
+    public int longestConsecutive(int[] nums) {
+        int longest = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int count = 1;
+            int x = nums[i];
+            while (linearSearch(nums, x + 1) == true) {
+                x = x + 1;
+                count = count + 1;
+            }
+            if (count > longest) {
+                longest = count;
+            }
+        }
+        return longest;
+    }
+    
+    private boolean linearSearch(int[] A, int key){
+        for(int ele: A){
+            if(ele == key){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+class Solution {        
+    // BETTER
+    // T: O(nlogn), S: O(1)
+    public int longestConsecutive(int[] nums) {
+        Arrays.sort(nums);
+
+        int lastSmaller = Integer.MIN_VALUE;
+        int longest = 0, n = nums.length, count = 0;
+
+        for (int i = 0; i < n; i++) {
+            // always update last smaller in case of sequence or no sequence
+            // in case when nums[i] == lastSmaller, then, it is duplicate element and we do nothing and go to next iteration
+            if (nums[i] - 1 == lastSmaller) {
+                count += 1;
+                lastSmaller = nums[i];
+            } else if (nums[i] != lastSmaller) {
+                count = 1;
+                lastSmaller = nums[i];
+            }
+            longest = Math.max(longest, count);
+        }
+        return longest;
+    }
+}
+
+class Solution {
+    // OPTIMAL
+    // refer STRIVER's Video
+    // Optimal - O(n) time and space both
+    // the iteration in inner while is max. n so O(n+n+n = n) time
     public int longestConsecutive(int[] nums) {
         Set<Integer> set = new HashSet<>();
         int longest = 0;
@@ -14,7 +72,7 @@ class Solution {
         // iterate thru all elements in set
         for(int num : set){
             int x = 1, count=1;
-            // check if num is a valid starting point, i.e., there is no 'num-1' smaller than num in the set
+            // check if num is a valid starting point, i.e., there is no 'num-1' in the set
             if(!set.contains(num-1)){
                 while(set.contains(num+x)){
                     x += 1;
--- Arrays And Hashing/ProductOfArrayExceptSelf.java
+++ Arrays And Hashing/ProductOfArrayExceptSelf.java
@@ -1,11 +1,34 @@
 class Solution {
-    // T: O(n), S: O(2n = n) - not the most optimal approach 
+    // BRUTE FORCE
+    // T: O(n^2), S: O(1)
+    // TLE
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int product = 1;
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    continue;
+                }
+                product *= nums[j];
+            }
+            res[i] = product;
+        }
+        return res;
+    }
+}
+
+class Solution {
+    // BETTER
+    // T: O(n), S: O(2n = n)
     public int[] productExceptSelf(int[] nums) {
         
         int n = nums.length;
         int[] answer = new int[n];
-        int[] lp = new int[n];
-        int[] rp = new int[n];
+        int[] lp = new int[n]; //left product = product of ele. before i
+        int[] rp = new int[n]; //right product = product of ele. after i
         
         // handling corner cases
         lp[0] = 1;
@@ -27,30 +50,32 @@ class Solution {
 }
 
 /*
- * The optimal approach using O(1) extra space
+ * OPTIMAL, T: O(n), S: O(1) extra space
  */
 class Solution {
-    // T: O(n), S: O(1) - constant space
-        public int[] productExceptSelf(int[] nums) {
-        
+    public int[] productExceptSelf(int[] nums) {
+
         int n = nums.length;
         int[] answer = new int[n];
         int left = 1, right = 1;
         // initializing corner elements
         answer[0] = 1;
-        answer[n-1] = 1;
-        
-        // building answer with left product, that is answer[i] = product of num before i;
-        for(int i=1; i<n; i++){
-            left = left * nums[i-1];
+        answer[n - 1] = 1;
+
+        // building answer with left product, that is answer[i] = product of num before
+        // i;
+        for (int i = 1; i < n; i++) {
+            left = left * nums[i - 1];
             answer[i] = left;
         }
-        // building answer with right product, that is, we have left product already in answer[i], so multiply it with right product of i to get final answer 
-        for(int i=n-2; i>=0; i--){
-            right = right * nums[i+1];
+        // building answer with right product, that is, we have left product already in
+        // answer[i], so multiply it with right product of i to get final answer
+        for (int i = n - 2; i >= 0; i--) {
+            right = right * nums[i + 1];
             answer[i] = answer[i] * right;
         }
-        // this handles corner cases as answer[n-1] is updating in the first iteration and ans[0] is updated in the second iteration
+        // this handles corner cases as answer[n-1] is updating in the first iteration
+        // and ans[0] is updated in the second iteration
         return answer;
     }
 }
