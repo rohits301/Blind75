00b2521ef5c45ec015a5d068c35b5aa598441d74
Rohit Sachdeve <sachdevarohit5@gmail.com>
2024-02-01 22:57:50 +0530

    Added Brute and better approach for 3Sum

--- Two Pointers/3Sum.java
+++ Two Pointers/3Sum.java
@@ -1,5 +1,63 @@
 class Solution {
-    // T: O(n^2 + nlogn), S: O(no. of triplets) - OPTIMAL
+    // refer STRIVER's VIDEO
+    // BRUTE FORCE
+    // T: O(n^3), S: O(no. of unique triplets)
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Set<List<Integer>> set = new HashSet();
+
+        int n = nums.length;
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        List<Integer> temp = new ArrayList<>();
+                        Collections.addAll(temp, nums[i], nums[j], nums[k]);
+                        Collections.sort(temp);
+                        set.add(temp);
+                    }
+                }
+            }
+        }
+
+        for (List<Integer> list : set) {
+            ans.add(list);
+        }
+        return ans;
+    }
+}
+
+class Solution {
+    // BETTER
+    // T: O(n^2) 
+    // S: O(n + no. of unique ele * 2); n=for hashset, no. of unique ele - first is for set, second is for the ans list
+    // using set, an external DS to store the answer 
+    public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        Set<List<Integer>> set = new HashSet<>();
+
+        for (int i = 0; i < n - 1; i++) {
+            Set<Integer> hashset = new HashSet<>();
+            for (int j = i + 1; j < n; j++) {
+
+                int third = -(nums[i] + nums[j]);
+                if (hashset.contains(third)) {
+                    List<Integer> temp = new ArrayList<>();
+                    Collections.addAll(temp, nums[i], nums[j], third);
+                    Collections.sort(temp);
+                    set.add(temp);
+                }
+                hashset.add(nums[j]);
+            }
+        }
+        return new ArrayList<>(set); // the answer list
+    }
+}
+
+class Solution {
+    // OPTIMAL
+    // T: O(n^2 + nlogn), S: O(no. of triplets) 
     // STRIVER video solution for Brute, Better and Optimal
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
