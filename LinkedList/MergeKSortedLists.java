36d234babbb5cd8a1186b0ac447213b98318b64e
Rohit Sachdeve <sachdevarohit5@gmail.com>
2024-02-23 15:23:06 +0530

    Merge K sorted lists - using merge sort approach

--- LinkedList/MergeKSortedLists.java
+++ LinkedList/MergeKSortedLists.java
@@ -78,3 +78,49 @@ class Solution {
         return dummy.next;
     }
 }
+
+class Solution {
+    // BETTER
+    // T: O(n*k*logk), S: O(n*k) - recursive stack
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+
+        return mergeKSortedListsHelper(lists, 0, lists.length - 1);
+    }
+
+    public ListNode mergeKSortedListsHelper(ListNode[] lists, int start, int end) {
+        if (start == end) {
+            return lists[start];
+        }
+
+        int mid = start + (end - start) / 2;
+        ListNode left = mergeKSortedListsHelper(lists, start, mid);
+        ListNode right = mergeKSortedListsHelper(lists, mid + 1, end);
+        return merge(left, right);
+    }
+
+    public ListNode merge(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                temp.next = list1;
+                temp = list1;
+                list1 = list1.next;
+            } else {
+                temp.next = list2;
+                temp = list2;
+                list2 = list2.next;
+            }
+        }
+        if (list1 != null) {
+            temp.next = list1;
+        } else {
+            temp.next = list2;
+        }
+        return dummy.next;
+    }
+}
