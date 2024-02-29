12548f1500b59e3736b78196f2566c74cad67e0e
Rohit Sachdeve <sachdevarohit5@gmail.com>
2024-02-29 16:56:10 +0530

    Lowest Common Ancestor in Binary Search Tree - brute, better and optimal

--- Trees/LowestCommonAncestorInBST.java
+++ Trees/LowestCommonAncestorInBST.java
@@ -9,6 +9,32 @@
  */
 
  class Solution {
+    // BRUTE FORCE
+    // refer STRIVER's LCA of binary tree video
+    // using the Binary tree approach on BST
+    // T: O(height), S: O(height) - space is recursion auxillary space
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // base case
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left == null) {
+            return right;
+        } else if (right == null) {
+            return left;
+        } else {
+            // both are non-null
+            return root;
+        }
+    }
+}
+
+class Solution {
+    // BETTER
     // refer STRIVER
     // Recursive
     // T: O(height), S: O(height), height = log n in case of BST
@@ -26,5 +52,29 @@
             return lowestCommonAncestor(root.left, p, q);
         }
         return root;
+    }
+}
+
+class Solution {
+    // OPTIMAL
+    // refer NEETCODE
+    // Iterative
+    // T: O(height), S: O(1), height = log n in case of BST
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return root;
+        }
+
+        TreeNode curr = root;
+        while (curr != null) {
+            if (curr.val < p.val && curr.val < q.val) {
+                curr = curr.right;
+            } else if (curr.val > p.val && curr.val > q.val) {
+                curr = curr.left;
+            } else {
+                return curr;
+            }
+        }
+        return curr; // case when curr is null
     }
 }
