1c7d880862d50ea110074cfb887f1a077524aa69
Rohit Sachdeve <sachdevarohit5@gmail.com>
2024-03-10 12:40:17 +0530

    Added tc and sc for Word Search-I and brute and optimal for Word Search-II

--- Backtracking/WordSearch.java
+++ Backtracking/WordSearch.java
@@ -1,10 +1,12 @@
 class Solution {
     // refer NEETCODE
-    // T: O(m*n*lengthOfWord), worst case lengthOfWord = m*n , S: O(m*n)
+    // T: O(m*n*4^lengthOfWord), worst case lengthOfWord = m*n
+    // S: O(lengthOfWord) - recursion stack space
     public boolean exist(char[][] board, String word) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                if (board[i][j] == word.charAt(0) && dfs(board, i, j, word, 0)) {
+                if (board[i][j] == word.charAt(0) && 
+                    dfs(board, i, j, word, 0)) {
                     return true;
                 }
             }
@@ -32,7 +34,7 @@ class Solution {
                   dfs(board, i + 1, j, word, idx + 1) ||
                   dfs(board, i, j + 1, word, idx + 1);
 
-            board[i][j] = temp;
+            board[i][j] = temp; // unvisit
         }
         return res;
     }
---
+++ Tries/WordSearchII.java
@@ -0,0 +1,119 @@
+class Solution {
+    // refer NEETCODE
+    // BRUTE FORCE
+    // TLE
+    // T: O(w*(m*n)*4^L), L=length of word, w = no. of words in words[]
+    // S: O(L) - recursion stack space
+    // use set to avoid returning multiple occurrences of the same word in the board, 
+    // e.g. words["oa", "oaa"] in the board of 
+    public List<String> findWords(char[][] board, String[] words) {
+        Set<String> set = new HashSet<>();
+
+        for (String word : words) {
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board[0].length; j++) {
+                    if (board[i][j] == word.charAt(0) &&
+                        dfs(board, i, j, word, 0)) {
+                        set.add(word);
+                    }
+                }
+            }
+        }
+        return new ArrayList<>(set);
+    }
+
+    private boolean dfs(char[][] board, int i, int j, String word, int idx) {
+        if (idx == word.length()) {
+            return true;
+        }
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length
+            || board[i][j] != word.charAt(idx)) {
+            return false;
+        }
+        
+        boolean res = false;
+        if (board[i][j] == word.charAt(idx)) {
+            char temp = board[i][j];
+            board[i][j] = '#';
+
+            res = dfs(board, i - 1, j, word, idx + 1) ||
+                  dfs(board, i, j - 1, word, idx + 1) ||
+                  dfs(board, i + 1, j, word, idx + 1) ||
+                  dfs(board, i, j + 1, word, idx + 1);
+
+            board[i][j] = temp;
+        }
+        return res;
+    }
+}
+
+class Solution {
+    // refer CodingDecoded and LeetCode discuss
+    // OPTIMAL
+    // T: O(m*n*4^L), S: O(L*words)
+    // L= average length of word, words = no. of words
+    private class TrieNode {
+        private TrieNode[] children;
+        private String word;
+
+        public TrieNode() {
+            children = new TrieNode[26];
+        }
+    }
+
+    public TrieNode buildTrie(String[] words) {
+        TrieNode root = new TrieNode();
+
+        for (String word : words) {
+            TrieNode curr = root;
+            for (char ch : word.toCharArray()) {
+                if (curr.children[ch - 'a'] == null) {
+                    curr.children[ch - 'a'] = new TrieNode();
+                }
+                curr = curr.children[ch - 'a'];
+            }
+            curr.word = word;
+        }
+        return root;
+    }
+
+    public List<String> findWords(char[][] board, String[] words) {
+        TrieNode root = buildTrie(words);
+        List<String> res = new ArrayList<>();
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                dfs(board, i, j, root, res);
+            }
+        }
+        return res;
+    }
+
+    public void dfs(char[][] board, int i, int j, TrieNode curr, List<String> res) {
+        char ch = board[i][j];
+        if (ch == '#' || curr.children[ch - 'a'] == null) {
+            return;
+        }
+
+        curr = curr.children[ch - 'a'];
+        if (curr.word != null) {
+            res.add(curr.word); // found the word
+            curr.word = null; // remove the word from trie to avoid duplication
+        }
+
+        board[i][j] = '#'; // mark visited
+        if (i > 0) {
+            dfs(board, i - 1, j, curr, res);
+        }
+        if (j > 0) {
+            dfs(board, i, j - 1, curr, res);
+        }
+        if (i < board.length - 1) {
+            dfs(board, i + 1, j, curr, res);
+        }
+        if (j < board[0].length - 1) {
+            dfs(board, i, j + 1, curr, res);
+        }
+        board[i][j] = ch; // unvisit
+    }
+}
