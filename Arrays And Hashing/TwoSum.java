9d1b11845b47e76df85a0a95f96ac983656eb30c
Rohit Sachdeve <sachdevarohit5@gmail.com>
2024-04-15 22:33:55 +0530

    Brute and better approach added for two-sum problem

--- Arrays And Hashing/TwoSum.java
+++ Arrays And Hashing/TwoSum.java
@@ -1,5 +1,47 @@
 class Solution {
-    // T: O(n), S: O(n)
+    // BRUTE FORCE
+    // T: O(n^2), S: O(n)
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    ans[0] = i;
+                    ans[1] = j;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
+
+class Solution {
+    // BETTER
+    // T: O(n), S: O(n) - two pass
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                ans[0] = i;
+                ans[1] = map.get(complement);
+                break;
+            }
+        }
+        return ans;
+    }
+}
+
+class Solution {
+    // OPTIMAL
+    // T: O(n), S: O(n) - one pass
     public int[] twoSum(int[] nums, int target) {
         int[] ans = new int[2];
         HashMap<Integer, Integer> map = new HashMap<>();
@@ -13,7 +55,6 @@ class Solution {
                 map.put(nums[i], i);
             }
         }
-
         return ans;
     }
 }
