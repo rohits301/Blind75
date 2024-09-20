1772971bc9d8842321e251976f51f6e044f526e3
LL\rohsachd2 <rohit.sachdeva@publicissapient.com>
2024-09-20 14:43:48 +0530

    Update: enhanced readability

--- Backtracking/WordSearch.java
+++ Backtracking/WordSearch.java
@@ -1,7 +1,7 @@
 class Solution {
     // refer NEETCODE
-    // T: O(m*n*4^lengthOfWord), worst case lengthOfWord = m*n
-    // S: O(lengthOfWord) - recursion stack space
+    // T: O(m*n*4^(lengthOfWord)); worst case lengthOfWord = m*n
+    // S: O(lengthOfWord); recursion stack space
     public boolean exist(char[][] board, String word) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
@@ -29,9 +29,9 @@ class Solution {
         board[i][j] = '#'; // mark visited
 
         res = dfs(board, i - 1, j, word, idx + 1) ||
-                dfs(board, i, j - 1, word, idx + 1) ||
-                dfs(board, i + 1, j, word, idx + 1) ||
-                dfs(board, i, j + 1, word, idx + 1);
+              dfs(board, i, j - 1, word, idx + 1) ||
+              dfs(board, i + 1, j, word, idx + 1) ||
+              dfs(board, i, j + 1, word, idx + 1);
 
         board[i][j] = temp; // unvisit
         return res;
