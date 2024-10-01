ba9663b62cc548ebba186b739836572d75d680ce
Rohit Sachdeva <43935715+rohits301@users.noreply.github.com>
2024-10-02 00:06:34 +0530

    Add: House Robber - most optimized clean code

--- 1-D Dynamic Programming/HouseRobber.java
+++ 1-D Dynamic Programming/HouseRobber.java
@@ -30,7 +30,7 @@ class Solution {
     // S: O(n) + O(n); stack space + dp array space
     public int rob(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n+1];
+        int[] dp = new int[n];
         Arrays.fill(dp, -1);
         return helper(n - 1, dp, nums);
     }
@@ -84,7 +84,7 @@ class Solution {
     // we only need last two to calculate the answer
     public int rob(int[] nums) {
         int n = nums.length;
-        int prev = nums[0];
+        int prev1 = nums[0];
         int prev2 = 0;
 
         for (int i = 1; i < n; i++) {
@@ -92,12 +92,12 @@ class Solution {
             if (i > 1) {
                 pick += prev2;
             }
-            int notPick = 0 + prev;
+            int notPick = 0 + prev1;
             int curr = Math.max(pick, notPick);
-            prev2 = prev;
-            prev = curr;
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return prev;
+        return prev1;
     }
 }
 
