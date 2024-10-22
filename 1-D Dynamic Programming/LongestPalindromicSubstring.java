c3daa6da4cf3378a6879789000e16e6347267be7
Rohit Sachdeva <sachdevarohit5@gmail.com>
2024-10-22 13:00:45 +0530

    Longest Palindromic Substring - recursion + memo.

--- 1-D Dynamic Programming/LongestPalindromicSubstring.java
+++ 1-D Dynamic Programming/LongestPalindromicSubstring.java
@@ -1,5 +1,51 @@
 class Solution {
-    // BRUTE FORCE - passed
+    // Brute -> Recursion, Memoization -> Better
+    // BETTER
+    // recursion + memoization
+    // T: O(n^3); O(n^2) for loop, O(n) for recursion on string
+    // S: O(n^2); O(n^2) for dp table + O(n) auxilary space for recursion
+    public String longestPalindrome(String s) {
+        int maxLength = 0;
+        int start = 0;
+        int[][] dp = new int[s.length()][s.length()];
+        for (int[] ar : dp) {
+            Arrays.fill(ar, -1);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i; j < s.length(); j++) {
+                if (solve(s, i, j, dp)) {
+                    if (j - i + 1 > maxLength) {
+                        maxLength = j - i + 1;
+                        start = i;
+                    }
+                }
+            }
+        }
+        return s.substring(start, start + maxLength);
+    }
+
+    private boolean solve(String s, int i, int j, int[][] dp) {
+        if (i >= j) {
+            return true;
+        }
+
+        if (dp[i][j] != -1) {
+            return dp[i][j] == 0 ? false : true;
+        }
+        if (s.charAt(i) == s.charAt(j)) {
+            boolean val = solve(s, i + 1, j - 1, dp);
+            dp[i][j] = val == false ? 0 : 1;
+            return val;
+        }
+        dp[i][j] = 0;
+        return false;
+    }
+}
+
+class Solution {
+    // Iteration Brute Force (with improvements) - passed
+    // EVEN BETTER
     // LEETCODE EDITORIAL
     // we check the biggest length substring first, followed by smaller length becoz. we are interested in largest length
     // there is 1 substring of len=n, 2 of len=n-1, 3 of len=n-2 ... and n of len=1, but we don't check them all, we return from wherever we find the largest length substring
