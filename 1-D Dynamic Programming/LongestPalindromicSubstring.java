47978d113c75f9f0e9b82c226b2649d29ebcd02d
Rohit Sachdeva <sachdevarohit5@gmail.com>
2024-10-24 23:42:46 +0530

    feat: LPS DP solution

--- 1-D Dynamic Programming/LongestPalindromicSubstring.java
+++ 1-D Dynamic Programming/LongestPalindromicSubstring.java
@@ -76,3 +76,44 @@ class Solution {
         return true;
     }
 }
+
+class Solution {
+    // refer LEETCODE EDITORIAL
+    // Better (DP - Tabulation) 
+    // T: O(n^2); time to fill the n^2 dp 
+    // S: O(n^2); space of dp array
+    public String longestPalindrome(String s) {
+        int[] ans = new int[2];
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+
+        // 1 - length substrings
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+            ans[0] = i;
+            ans[1] = i;
+        }
+        // 2 - length substrings
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = true;
+                ans[0] = i;
+                ans[1] = i + 1;
+            }
+        }
+        // 3 and more - length substrings
+        for (int diff = 2; diff < n; diff++) {
+            for (int i = 0; i < n - diff; i++) {
+                int j = i + diff;
+                if (s.charAt(i) == s.charAt(j)
+                    && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                    ans[0] = i;
+                    ans[1] = j;
+                }
+            }
+        }
+        int i = ans[0], j = ans[1];
+        return s.substring(i, j + 1);
+    }
+}
