b9f880ea93eb025280c61f1649c1ee65447cacda
Rohit Sachdeva <43935715+rohits301@users.noreply.github.com>
2025-02-21 09:56:01 +0530

    Update SubtreeOfAnotherTree.java

--- Trees/SubtreeOfAnotherTree.java
+++ Trees/SubtreeOfAnotherTree.java
@@ -14,28 +14,32 @@
  * }
  */
 class Solution {
-    // refer NEETCODE
-    // T: O(s*t), S: O(s+t), s = no. of nodes in root tree, t = no. of nodes in subRoot tree
+    // T: O(n1 * n2) - sum of nodes in root & subRoot trees
+    // S: O(h1 + h2) - sum of heights is the stack size
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if (subRoot == null)
-            return true;
-        if (root == null)
-            return false;
-
-        if (isSameTree(root, subRoot)) {
+        if(subRoot == null){
             return true;
         }
 
-        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+        if(root == null){
+            return false;
+        }
+
+        if(isSameTree(root, subRoot)){
+            return true;
+        }
+
+        return (isSubtree(root.left, subRoot) || 
+                isSubtree(root.right, subRoot));
     }
 
-    private boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null || q == null) {
+    private boolean isSameTree(TreeNode p, TreeNode q){
+        if(p == null || q == null){
             return (p == null && q == null);
         }
 
-        return p.val == q.val &&
-                isSameTree(p.left, q.left) &&
-                isSameTree(p.right, q.right);
+        return ((p.val == q.val) && 
+                isSameTree(p.left, q.left) && 
+                isSameTree(p.right, q.right));
     }
 }
