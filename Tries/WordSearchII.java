efe5538421c0240ba0eb95b552ae67bbe428474c
Rohit Sachdeva <43935715+rohits301@users.noreply.github.com>
2025-03-09 00:45:11 +0530

    Update WordSearchII.java

--- Tries/WordSearchII.java
+++ Tries/WordSearchII.java
@@ -81,37 +81,40 @@ class Solution {
 
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                dfs(board, i, j, root, res);
+                dfs(i, j, boardroot, res);
             }
         }
         return res;
     }
-
-    public void dfs(char[][] board, int i, int j, TrieNode curr, List<String> res) {
-        char ch = board[i][j];
-        if (ch == '#' || curr.children[ch - 'a'] == null) {
+    
+    private void dfs(int i, int j, char[][] board, TrieNode node, List<String> res) {
+        // Base conditions: Check bounds and visited cells
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] == '#') {
             return;
         }
 
-        curr = curr.children[ch - 'a'];
-        if (curr.word != null) {
-            res.add(curr.word); // found the word
-            curr.word = null; // remove the word from trie to avoid duplication
+        char ch = board[i][j];
+        if (node.children[ch - 'a'] == null){
+            return;
+        }
+        node = node.children[ch - 'a'];
+
+        // If we found a word, add it to result
+        if (node.word != null) {
+            res.add(node.word);
+            node.word = null; // Remove duplicate words
         }
 
-        board[i][j] = '#'; // mark visited
-        if (i > 0) {
-            dfs(board, i - 1, j, curr, res);
-        }
-        if (j > 0) {
-            dfs(board, i, j - 1, curr, res);
-        }
-        if (i < board.length - 1) {
-            dfs(board, i + 1, j, curr, res);
-        }
-        if (j < board[0].length - 1) {
-            dfs(board, i, j + 1, curr, res);
-        }
-        board[i][j] = ch; // unvisit
+        // Mark as visited
+        board[i][j] = '#';
+
+        // Explore in all 4 directions
+        dfs(i - 1, j, board, node, res); // Up
+        dfs(i + 1, j, board, node, res); // Down
+        dfs(i, j - 1, board, node, res); // Left
+        dfs(i, j + 1, board, node, res); // Right
+
+        // Restore the board for backtracking
+        board[i][j] = ch;
     }
 }
