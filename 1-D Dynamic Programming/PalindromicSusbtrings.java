468dfa8bc1cafe094a0e4d44c9ab426b084a7493
Rohit Sachdeva <43935715+rohits301@users.noreply.github.com>
2025-03-09 23:38:26 +0530

    Update PalindromicSusbtrings.java

--- 1-D Dynamic Programming/PalindromicSusbtrings.java
+++ 1-D Dynamic Programming/PalindromicSusbtrings.java
@@ -1,5 +1,46 @@
 class Solution {
+    // BETTER
+    // T: O(n^2)
+    // S: O(n^2)
+    // DP approach
+    public int countSubstrings(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int ans = 0;
+
+        // 1
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+            ans++;
+        }
+
+        // 2
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = true;
+                ans++;
+            }
+        }
+
+        // 3 and more
+        for (int diff = 2; diff < n; diff++) {
+            for (int i = 0; i < n - diff; i++) {
+                int j = i + diff;
+                if (s.charAt(i) == s.charAt(j) &&
+                        dp[i + 1][j - 1] == true) {
+                    dp[i][j] = true;
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
+
+class Solution {
     // Basically, an extension to the longest palindromic substring
+    // OPTIMAL
     // T: O(n^2), S: O(1)
     // refer Neetcode
     public int countSubstrings(String s) {
